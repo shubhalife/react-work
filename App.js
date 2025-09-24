2a4ae932d4d56fe57df5a2e37735b42290575c6e
ecgcdevops <ecgcdevops@cdac.in>
2025-09-24 23:20:27 +0530

    chapter 2 complted

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-{
-  const heading = React.createElement("h1", { id: "heading" }, "hello World");
-  const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement("h1", { id: "heading" }, "Hello welcome");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-  root.render(heading);
-}
+root.render(heading);
